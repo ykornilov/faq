2e2d51b440c6165fef5537c4c3e938b174d3c9aa
Yury Kornilov <kornilovyv81@gmail.com>
2017-11-07 06:19:41 +0300

    faq.sql is updated

--- faq.sql
+++ faq.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Время создания: Окт 23 2017 г., 04:09
+-- Время создания: Ноя 07 2017 г., 03:00
 -- Версия сервера: 5.7.14
 -- Версия PHP: 5.6.25
 
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- База данных: `faq`
+-- База данных: `faq_test`
 --
 
 -- --------------------------------------------------------
@@ -32,16 +32,14 @@ CREATE TABLE `authors` (
   `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Дамп данных таблицы `authors`
 --
 
 INSERT INTO `authors` (`id`, `name`, `email`, `created_at`, `updated_at`) VALUES
-(1, 'Ivan', 'ivan@ivanov.ru', '2017-10-16 13:08:53', '2017-10-16 13:08:53'),
-(2, 'Petr', 'petr@petrov.ru', '2017-10-16 13:30:27', '2017-10-17 07:48:21'),
-(3, 'Sidor', 'sidor@sidorov.ru', '2017-10-17 17:39:42', '2017-10-17 17:39:42');
+(1, 'Ivan', 'ivan@ivanov.ru', '2017-11-07 02:51:20', '2017-11-07 02:51:20');
 
 -- --------------------------------------------------------
 
@@ -55,15 +53,15 @@ CREATE TABLE `categories` (
   `user_id` int(11) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Дамп данных таблицы `categories`
 --
 
 INSERT INTO `categories` (`id`, `title`, `user_id`, `created_at`, `updated_at`) VALUES
-(1, 'Basics', 1, '2017-10-16 09:37:39', '2017-10-16 09:37:39'),
-(2, 'Mobile', 1, '2017-10-16 09:38:19', '2017-10-16 09:38:19');
+(1, 'Basics', 1, '2017-11-07 02:50:12', '2017-11-07 02:50:12'),
+(2, 'Mobile', 1, '2017-11-07 02:50:31', '2017-11-07 02:50:31');
 
 -- --------------------------------------------------------
 
@@ -82,11 +80,11 @@ CREATE TABLE `migrations` (
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(3, '2014_10_12_000000_create_users_table', 1),
-(4, '2014_10_12_100000_create_password_resets_table', 1),
-(5, '2017_10_15_185857_create_categories_table', 2),
-(6, '2017_10_15_190945_create_questions_table', 2),
-(7, '2017_10_15_191036_create_authors_table', 2);
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_resets_table', 1),
+(3, '2017_10_15_185857_create_categories_table', 1),
+(4, '2017_10_15_190945_create_questions_table', 1),
+(5, '2017_10_15_191036_create_authors_table', 1);
 
 -- --------------------------------------------------------
 
@@ -112,22 +110,24 @@ CREATE TABLE `questions` (
   `answer` text COLLATE utf8mb4_unicode_ci,
   `is_published` tinyint(1) NOT NULL DEFAULT '0',
   `is_blocked` tinyint(1) NOT NULL DEFAULT '0',
-  `category_id` int(11) NOT NULL,
+  `category_id` int(10) UNSIGNED NOT NULL,
   `author_id` int(11) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Дамп данных таблицы `questions`
 --
 
 INSERT INTO `questions` (`id`, `question`, `answer`, `is_published`, `is_blocked`, `category_id`, `author_id`, `created_at`, `updated_at`) VALUES
-(1, 'How do I change my password?', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae quidem blanditiis delectus corporis, possimus officia sint sequi ex tenetur id impedit est pariatur iure animi non a ratione reiciendis nihil sed consequatur atque repellendus fugit perspiciatis rerum et. Dolorum consequuntur fugit deleniti, soluta fuga nobis. Ducimus blanditiis velit sit iste delectus obcaecati debitis omnis, assumenda accusamus cumque perferendis eos aut quidem! Aut, totam rerum, cupiditate quae aperiam voluptas rem inventore quas, ex maxime culpa nam soluta labore at amet nihil laborum? Explicabo numquam, sit fugit, voluptatem autem atque quis quam voluptate fugiat earum rem hic, reprehenderit quaerat tempore at. Aperiam.', 1, 0, 1, 1, '2017-10-16 13:08:53', '2017-10-17 11:45:50'),
-(2, 'How do I sign up?', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi cupiditate et laudantium esse adipisci consequatur modi possimus accusantium vero atque excepturi nobis in doloremque repudiandae soluta non minus dolore voluptatem enim reiciendis officia voluptates, fuga ullam? Voluptas reiciendis cumque molestiae unde numquam similique quas doloremque non, perferendis doloribus necessitatibus itaque dolorem quam officia atque perspiciatis dolore laudantium dolor voluptatem eligendi? Aliquam nulla unde voluptatum molestiae, eos fugit ullam, consequuntur, saepe voluptas quaerat deleniti. Repellendus magni sint temporibus, accusantium rem commodi?', 1, 0, 1, 1, '2017-10-16 13:11:55', '2017-10-23 04:03:41'),
-(3, 'Can I remove a post?', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis provident officiis, reprehenderit numquam. Praesentium veritatis eos tenetur magni debitis inventore fugit, magnam, reiciendis, saepe obcaecati ex vero quaerat distinctio velit.', 0, 0, 1, 1, '2017-10-16 13:23:22', '2017-10-16 13:23:22'),
-(4, 'How does syncing work?', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit quidem delectus rerum eligendi mollitia, repudiandae quae beatae. Et repellat quam atque corrupti iusto architecto impedit explicabo repudiandae qui similique aut iure ipsum quis inventore nulla error aliquid alias quia dolorem dolore, odio excepturi veniam odit veritatis. Quo iure magnam, et cum. Laudantium, eaque non? Tempore nihil corporis cumque dolor ipsum accusamus sapiente aliquid quis ea assumenda deserunt praesentium voluptatibus, accusantium a mollitia necessitatibus nostrum voluptatem numquam modi ab, sint rem.', 0, 0, 2, 2, '2017-10-16 13:30:27', '2017-10-17 17:46:07'),
-(5, 'How do reviews work?', NULL, 1, 0, 1, 2, '2017-10-16 13:31:51', '2017-10-17 07:58:11');
+(1, 'How do I change my password?', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae quidem blanditiis delectus corporis, possimus officia sint sequi ex tenetur id impedit est pariatur iure animi non a ratione reiciendis nihil sed consequatur atque repellendus fugit perspiciatis rerum et. Dolorum consequuntur fugit deleniti, soluta fuga nobis. Ducimus blanditiis velit sit iste delectus obcaecati debitis omnis, assumenda accusamus cumque perferendis eos aut quidem! Aut, totam rerum, cupiditate quae aperiam voluptas rem inventore quas, ex maxime culpa nam soluta labore at amet nihil laborum? Explicabo numquam, sit fugit, voluptatem autem atque quis quam voluptate fugiat earum rem hic, reprehenderit quaerat tempore at. Aperiam.', 1, 0, 1, 1, '2017-11-07 02:51:20', '2017-11-07 02:55:25'),
+(2, 'How do I sign up?', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi cupiditate et laudantium esse adipisci consequatur modi possimus accusantium vero atque excepturi nobis in doloremque repudiandae soluta non minus dolore voluptatem enim reiciendis officia voluptates, fuga ullam? Voluptas reiciendis cumque molestiae unde numquam similique quas doloremque non, perferendis doloribus necessitatibus itaque dolorem quam officia atque perspiciatis dolore laudantium dolor voluptatem eligendi? Aliquam nulla unde voluptatum molestiae, eos fugit ullam, consequuntur, saepe voluptas quaerat deleniti. Repellendus magni sint temporibus, accusantium rem commodi?', 1, 0, 1, 1, '2017-11-07 02:52:01', '2017-11-07 02:55:49'),
+(3, 'Can I remove a post?', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis provident officiis, reprehenderit numquam. Praesentium veritatis eos tenetur magni debitis inventore fugit, magnam, reiciendis, saepe obcaecati ex vero quaerat distinctio velit.', 0, 0, 1, 1, '2017-11-07 02:52:29', '2017-11-07 02:56:08'),
+(4, 'How do reviews work?', NULL, 0, 0, 1, 1, '2017-11-07 02:52:58', '2017-11-07 02:52:58'),
+(5, 'How does syncing work?', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit quidem delectus rerum eligendi mollitia, repudiandae quae beatae. Et repellat quam atque corrupti iusto architecto impedit explicabo repudiandae qui similique aut iure ipsum quis inventore nulla error aliquid alias quia dolorem dolore, odio excepturi veniam odit veritatis. Quo iure magnam, et cum. Laudantium, eaque non? Tempore nihil corporis cumque dolor ipsum accusamus sapiente aliquid quis ea assumenda deserunt praesentium voluptatibus, accusantium a mollitia necessitatibus nostrum voluptatem numquam modi ab, sint rem.', 0, 0, 2, 1, '2017-11-07 02:53:37', '2017-11-07 02:56:51'),
+(6, 'How do I upload files from my phone or tablet?', NULL, 0, 0, 2, 1, '2017-11-07 02:54:09', '2017-11-07 02:54:09'),
+(7, 'How do I link to a file or folder?', NULL, 0, 0, 2, 1, '2017-11-07 02:54:35', '2017-11-07 02:54:35');
 
 -- --------------------------------------------------------
 
@@ -142,15 +142,14 @@ CREATE TABLE `users` (
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Дамп данных таблицы `users`
 --
 
 INSERT INTO `users` (`id`, `login`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'admin', '$2y$10$fnDw0FszTIarnY.6RCmY9.Gd7jgl.mXXpuL1Y5vqzbB7wTkcGVw0q', 'wKjlMpMraX2kq0yOB74uLuDL1aiTsp3utihzrYNyqiaJ4PBf0ppSQM6qUvRe', '2017-10-14 07:13:17', '2017-10-14 07:13:17'),
-(3, 'admin1', '$2y$10$X4vn7uWJUd.qsYzEacLcneKcGFSp.PsOHwUmgnG..9GsZ08Q4bL6C', 'oc9HgRTLM7y8sb1xyzLOdFP4Aq24oiVtjsnhWa4A63DByou8DaZ7lnDNs8SQ', '2017-10-14 08:48:49', '2017-10-15 08:17:31');
+(1, 'admin', '$2y$10$CjlE.QEjQFs5sKKgO/nJCeQqe5TJ5NBAvQCxNRNgUGIW8Elv85hMW', 'NLMQ8IAVoRqOb4P8Y1GdoC9G6MWiFbE3eEVu2VplZD1wYTKECITi3HOwwoQb', '2017-11-07 02:48:34', '2017-11-07 02:48:34');
 
 --
 -- Индексы сохранённых таблиц
@@ -186,7 +185,8 @@ ALTER TABLE `password_resets`
 -- Индексы таблицы `questions`
 --
 ALTER TABLE `questions`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `questions_category_id_foreign` (`category_id`);
 
 --
 -- Индексы таблицы `users`
@@ -203,27 +203,37 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `authors`
 --
 ALTER TABLE `authors`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT для таблицы `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT для таблицы `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT для таблицы `questions`
 --
 ALTER TABLE `questions`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `questions`
+--
+ALTER TABLE `questions`
+  ADD CONSTRAINT `questions_category_id_foreign` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
